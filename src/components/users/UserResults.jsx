40a36b9586fa42fb9c7290c1bb19b88b99ea9429
Abeeb <abeebayinla@gmail.com>
2022-07-11 03:33:46 +0100

    first commit

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from '../users/UserItem';
+import GithubContext from '../../context/github/GithubContext';
+
+const UserResults = () => {
+  const { users, loading } = useContext(GithubContext);
+
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+export default UserResults;

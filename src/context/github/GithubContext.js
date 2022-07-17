1b0ffd0892fbfdaddbc1383b05f8406da8903f1b
Abeeb <abeebayinla@gmail.com>
2022-07-17 15:06:41 +0100

    feat: added repos to state

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -2,69 +2,22 @@ import { createContext, useReducer } from 'react';
 import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // search users
-  const searchUsers = async (text) => {
-    setLoading();
-    const params = new URLSearchParams({
-      q: text,
-    });
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      Authorization: `Bearer ${GITHUB_TOKEN}`,
-    });
-    const { items } = await response.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    });
-  };
-
-  // get single user
-  const getUser = async (login) => {
-    setLoading();
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      Authorization: `Bearer ${GITHUB_TOKEN}`,
-    });
-
-    // redirect to 404 page if item not match
-    if (response.status === 404) {
-      window.location = '/notfound';
-    } else {
-      const data = await response.json();
-
-      dispatch({
-        type: 'GET_USER',
-        payload: data,
-      });
-    }
-  };
-
-  const clearUsers = async () => dispatch({ type: 'CLEAR_USERS' });
-
-  // set loading
-  const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        searchUsers,
-        clearUsers,
-        getUser,
+        ...state,
+        dispatch,
       }}
     >
       {children}
